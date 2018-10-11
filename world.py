f09f4e843ac3fdb8140418030babf70b3747aadd
Luke Harold Miles <luke@cs.uky.edu>
2018-10-11 15:22:39 -0400

    added Location class and default args for Agent

--- world.py
+++ world.py
@@ -1,13 +1,19 @@
-
 class World(object):
     def __init__(self):
-        self.agents = [Actor("knight"), Actor("King"), Actor("Dragon"), Actor("Wizard")]
+        self.num_rows = 10
+        self.num_cols = 10
+        self.actors = [Actor("Knight"), Actor("King"),
+                       Actor("Dragon"), Actor("Wizard")]
+        self.items = [Item("Sword"), Item("Enchantment")]
+        self.locations = [Location("Swamp", ), Location("Castle"),
+                          Location("Cave"), Location("Forge")]
 
 
-class Agent(object):
-    def __init__(self, name):
+class Actor(object):
+    def __init__(self, name, inventory=None, location=None):
         self.name = name
-        self.inventory = []
+        self.inventory = inventory or []
+        self.location = location
 
 
 class Item(object):
@@ -15,3 +21,10 @@ class Item(object):
         self.name = name
         self.owner = None
         self.purpose = None
+
+
+class Location(object):
+    def __init__(self, name, coords, actors):
+        self.name = name
+        self.coords = coords
+        self.actors = []  # actors inside this location
