123e65ff43de1b180075b991765b7b110f01a2e1
Luke Harold Miles <luke@cs.uky.edu>
2019-01-01 14:30:48 -0500

    made classifier calculate scores for plot events

--- classifier.py
+++ classifier.py
@@ -1,10 +1,13 @@
 import pickle
 import random
 from sys import stdin
+
 import nltk
 import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
+
+from narrative_tree import tree
 
 with open('data.csv', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
@@ -21,23 +24,46 @@ token_lists = [[porterstemmer.stem(token.lower())
 all_tokens = sorted(set(t for tl in token_lists for t in tl))
 token_to_index = {t: i for i, t in enumerate(all_tokens)}
 
-x = np.zeros([len(token_lists), len(all_tokens)], bool)
+X = np.zeros([len(token_lists), len(all_tokens)], bool)
 for i, token_list in enumerate(token_lists):
     for token in token_list:
         j = token_to_index[token]
-        x[i][j] = True
+        X[i][j] = True
 
 classifier = LogisticRegression(solver='lbfgs')
-classifier.fit(x, labels)
+classifier.fit(X, labels)
 print("Full train accuracy:")
-print(classifier.score(x, labels))
+print(classifier.score(X, labels))
 
-print("Saving fully trained model...")
-with open('classifier.pickle', 'wb') as f:
-    pickle.dump(classifier, f)
+
+
+event_scores = dict()
+def get_event_scores(tree):
+    for event in tree:
+        if event in event_scores:
+            continue
+        tokens = [porterstemmer.stem(token.lower())
+                  for token in nltk.word_tokenize(event)]
+        x = np.zeros([1, len(all_tokens)], bool)
+        for t in tokens:
+            if t in token_to_index:
+                x[0, token_to_index[t]] = True
+        event_scores[event] = classifier.predict_proba(x)[0][0]
+    for subtree in tree.values():
+        get_event_scores(subtree)
+get_event_scores(tree)
+print("Event scores:")
+print(event_scores)
+
+
+# print("Saving fully trained model...")
+# with open('classifier.pickle', 'wb') as f:
+#     pickle.dump(classifier, f)
 
 kfold = KFold(n_splits=5)
-for split_num, (train_index, test_index) in enumerate(kfold.split(x)):
-    classifier.fit(x[train_index], labels[train_index])
+for split_num, (train_index, test_index) in enumerate(kfold.split(X)):
+    classifier.fit(X[train_index], labels[train_index])
     print("Split number", split_num, ":")
-    print(classifier.score(x[test_index], labels[test_index]))
+    print(classifier.score(X[test_index], labels[test_index]))
+
+
--- narrative_tree.py
+++ narrative_tree.py
@@ -1,58 +1,49 @@
 tree = {'leave swamp': {
-    'go to kingdom': {
+  'go to kingdom': {
+    'ask king for slip': {
+      'go to forge': {
+        'give blacksmith slip from king': {
+          'ask blacksmith for sword': {
+            'go to tower': {
+              'ask wizard for amulet': {
+                'go to cave': {
+                  'kill dragon': {}}}}}}}}},
+  'go to forge': {
+    'kill blacksmith': {  # with your hands
+      'steal sword': {
+        'go to tower': {
+          'steal amulet': {
+            'go to cave': {
+              'kill dragon': {}}}}}}},
+  'go to tower': {
+    'ask wizard for amulet': {
+      'go to kingdom': {
         'ask king for slip': {
-            'go to forge': {
-                'give blacksmith slip from king': {
-                    'ask blacksmith for sword': {
-                        'go to tower': {
-                            'ask wizard for amulet': {
-                                'go to cave': {
-                                    'kill dragon': {},
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    },
-    'go to forge': {
-        'kill blacksmith': {  # with your hands
-            'steal sword': {
-                'go to tower': {
-                    'steal amulet': {
-                        'go to cave': {
-                            'kill dragon': {},
-                        },
-                    },
-                },
-            },
-        },
-    },
-    'go to tower': {
-        'ask wizard for amulet': {
-            'go to kingdom': {
-                'ask king for slip': {
-                    'go to forge': {
-                        'give blacksmith slip from king': {
-                            'get sword from blacksmith': {
-                                'go to cave': {
-                                    'kill dragon': {},
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-            'go to forge': {
-                'kill blacksmith': {
-                    'steal sword': {
-                        'go to cave': {
-                            'kill dragon': {},
-                        },
-                    },
-                },
-            },
-        },
-    },
+          'go to forge': {
+            'give blacksmith slip from king': {
+              'get sword from blacksmith': {
+                'go to cave': {
+                  'kill dragon': {}}}}}}},
+      'go to forge': {
+        'kill blacksmith': {
+          'steal sword': {
+            'go to cave': {
+              'kill dragon': {}}}}}}},
 }}
+
+
+scores = {
+ 'leave swamp': 0.8814424016481601,
+ 'go to kingdom': 0.7181800326883012,
+ 'go to forge': 0.7181800326883012,
+ 'go to tower': 0.7181800326883012,
+ 'ask king for slip': 0.4325354159928857,
+ 'give blacksmith slip from king': 0.7189033747995304,
+ 'ask blacksmith for sword': 0.3917996288657737,
+ 'ask wizard for amulet': 0.3917996288657737,
+ 'go to cave': 0.7181800326883012,
+ 'kill dragon': 0.67316854858286,
+ 'kill blacksmith': 0.67316854858286,
+ 'steal sword': 0.67316854858286,
+ 'steal amulet': 0.67316854858286,
+ 'get sword from blacksmith': 0.6048948027429133}
