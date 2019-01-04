9e06523d503d159e856ed6a16df6973b6758426e
Luke Harold Miles <luke@cs.uky.edu>
2019-01-04 19:58:14 +0000

    switched to word2vec+lstm classifier

--- classifier.py
+++ classifier.py
@@ -1,69 +1,127 @@
-import pickle
-import random
-from sys import stdin
+import time
 
+import gensim.downloader
+import matplotlib.pyplot as plt
 import nltk
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.linear_model import LogisticRegression
+from tensorflow import keras
 
 from narrative_tree import tree
 
-with open('data.csv', 'r') as f:
-    lines = [line.strip() for line in f.readlines()]
-#lines = [line.strip() for line in stdin]
-labels = np.array([bool(int(line[-1])) for line in lines])
-lines = [line[:-1] for line in lines]
+word2vec = gensim.downloader.load("glove-twitter-25")
 
-porterstemmer = nltk.stem.PorterStemmer()
+def get_x_y():
+    "Load in all data"
+    with open('data.csv', 'r') as f:
+        lines = [line.strip().lower() for line in f.readlines()]
+    y = np.array([int(line[-1]) for line in lines], bool)
+    lines = [line[:-1] for line in lines]
 
-token_lists = [[porterstemmer.stem(token.lower())
-                for token in nltk.word_tokenize(line)]
-               for line in lines]
+    token_lists = [nltk.word_tokenize(line) for line in lines]
+    max_length = max(len(l) for l in token_lists)
+    x = [] # np.zeros([len(token_lists), max_length, 25], float)
+    for token_list in token_lists:
+        x.append([])
+        for token in token_list:
+            try: vec = word2vec.get_vector(token)
+            except KeyError: continue
+            x[-1].append(vec)
+            #X[i, -j-1] = vec
+    x = keras.preprocessing.sequence.pad_sequences(x, maxlen=None, dtype='float32')
+    return x, y
 
-all_tokens = sorted(set(t for tl in token_lists for t in tl))
-token_to_index = {t: i for i, t in enumerate(all_tokens)}
+def run_experiment(model_maker, x, y, epochs=20, batch_size=100, k=5):
+    "Given a model, train it and run cross validation and make plots"
+    start = time.time()
+    print("start time:", time.ctime())
+    name = model_maker.__name__
+    print("Running model", name, "three times...")
+    model = model_maker()
+    #config_dict = model.get_config()
+    histories = []
+    for i, xtr, ytr, xv, yv in k_fold_split(x, y, k):
+        print("Starting validation", i, "...")
+        model = model_maker()
+        histories.append(model.fit(xtr, ytr, batch_size, epochs, validation_data=(xv, yv), verbose=1))
+    make_acc_plot(histories, name)
+    make_loss_plot(histories, name)
+    model.summary()
+    print("finish time:", time.ctime())
+    print("Total time in seconds:", time.time() - start)
+    return histories
 
-X = np.zeros([len(token_lists), len(all_tokens)], bool)
-for i, token_list in enumerate(token_lists):
-    for token in token_list:
-        j = token_to_index[token]
-        X[i][j] = True
+def make_acc_plot(histories, title=None):
+    "Plot accuracy from keras training histories"
+    colors = [((.4,0,0),(.9,0,0)),((0,.4,0),(0,.9,0)),((0,0,.4),(0,0,.9)),((.4,.4,0),(.9,.9,0)),((0,.4,.4),(0,.9,.9)),((.4,0,.4),(.9,0,.9))]
+    for i, ((c1, c2), history) in enumerate(zip(colors,histories)):
+        acc_values = history.history['acc']
+        val_acc_values = history.history['val_acc']
+        epochs = range(1, len(acc_values) + 1)
+        plt.plot(epochs, acc_values, label='Training {}'.format(i), color=c1)
+        plt.plot(epochs, val_acc_values, label='Validation {}'.format(i), color=c2)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    for y in [.65, .7, .75, .8]:
+        plt.axhline(y=y, color='black', linestyle='dashed')
+    if title:
+        plt.title(title)
+    plt.show()
 
-classifier = LogisticRegression(solver='lbfgs')
-classifier.fit(X, labels)
-print("Full train accuracy:")
-print(classifier.score(X, labels))
+def make_loss_plot(histories, title=None):
+    "Plot loss from keras training histories"
+    colors = [((.4,0,0),(.9,0,0)),((0,.4,0),(0,.9,0)),((0,0,.4),(0,0,.9)),((.4,.4,0),(.9,.9,0)),((0,.4,.4),(0,.9,.9)),((.4,0,.4),(.9,0,.9))]
+    for i, ((c1, c2), history) in enumerate(zip(colors,histories)):
+        loss_values = history.history['loss']
+        val_loss_values = history.history['val_loss']
+        epochs = range(1, len(loss_values) + 1)
+        plt.plot(epochs, loss_values, label='Training {}'.format(i), color=c1)
+        plt.plot(epochs, val_loss_values, label='Validation {}'.format(i), color=c2)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    if title:
+        plt.title(title)
+    plt.show()
 
+def k_fold_split(x, y, k=5):
+    "Simple k-fold cross-validation"
+    n = len(x)
+    w = n // k
+    p = list(np.random.permutation(n))
+    for i in range(k):
+        tr = p[:i*w] + p[(i+1)*w:]
+        v = p[i*w:(i+1)*w]
+        yield i, x[tr], y[tr], x[v], y[v]
 
+def make_lstm():
+    "Make a basic recurrent classifier"
+    model = keras.models.Sequential()
+    model.add(keras.layers.LSTM(50))
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+x, y = get_x_y()
+
+run_experiment(make_lstm, x, y, epochs=50, batch_size=50, k=5)
+
+# Train one model on all data for predictions on the plot events
+model = make_lstm()
+model.fit(x, y, batch_size=50, epochs=35)
+
+def to_matrix(sentence):
+    return np.array([[word2vec.get_vector(t) for t in nltk.word_tokenize(sentence)]])
 
 event_scores = dict()
 def get_event_scores(tree):
     for event in tree:
         if event in event_scores:
             continue
-        tokens = [porterstemmer.stem(token.lower())
-                  for token in nltk.word_tokenize(event)]
-        x = np.zeros([1, len(all_tokens)], bool)
-        for t in tokens:
-            if t in token_to_index:
-                x[0, token_to_index[t]] = True
-        event_scores[event] = classifier.predict_proba(x)[0][0]
+        x = to_matrix(event)
+        event_scores[event] = model.predict_proba(x)[0,0]
     for subtree in tree.values():
         get_event_scores(subtree)
 get_event_scores(tree)
 print("Event scores:")
 print(event_scores)
-
-
-# print("Saving fully trained model...")
-# with open('classifier.pickle', 'wb') as f:
-#     pickle.dump(classifier, f)
-
-kfold = KFold(n_splits=5)
-for split_num, (train_index, test_index) in enumerate(kfold.split(X)):
-    classifier.fit(X[train_index], labels[train_index])
-    print("Split number", split_num, ":")
-    print(classifier.score(X[test_index], labels[test_index]))
-
-
