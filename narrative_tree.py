2baeff5001cfe72b2a52766ba8645e7bce127f08
Luke Harold Miles <luke@cs.uky.edu>
2019-01-04 20:20:50 +0000

    updated scores from logistic regression to the lstm stuff

--- narrative_tree.py
+++ narrative_tree.py
@@ -28,22 +28,21 @@ tree = {'leave swamp': {
         'kill blacksmith': {
           'steal sword': {
             'go to cave': {
-              'kill dragon': {}}}}}}},
-}}
-
+              'kill dragon': {}}}}}}}}}
 
 scores = {
- 'leave swamp': 0.8814424016481601,
- 'go to kingdom': 0.7181800326883012,
- 'go to forge': 0.7181800326883012,
- 'go to tower': 0.7181800326883012,
- 'ask king for slip': 0.4325354159928857,
- 'give blacksmith slip from king': 0.7189033747995304,
- 'ask blacksmith for sword': 0.3917996288657737,
- 'ask wizard for amulet': 0.3917996288657737,
- 'go to cave': 0.7181800326883012,
- 'kill dragon': 0.67316854858286,
- 'kill blacksmith': 0.67316854858286,
- 'steal sword': 0.67316854858286,
- 'steal amulet': 0.67316854858286,
- 'get sword from blacksmith': 0.6048948027429133}
+ 'steal sword': 0.4273727,
+ 'kill blacksmith': 0.4383431,
+ 'steal amulet': 0.4772043,
+ 'go to cave': 0.45955008,
+ 'go to forge': 0.49036837,
+ 'ask blacksmith for sword': 0.49806365,
+ 'go to tower': 0.45170587,
+ 'ask wizard for amulet': 0.5262374,
+ 'go to kingdom': 0.5286611,
+ 'leave swamp': 0.41815662,
+ 'get sword from blacksmith': 0.48302805,
+ 'kill dragon': 0.3663328,
+ 'ask king for slip': 0.5143399,
+ 'give blacksmith slip from king': 0.5256291,
+ }
